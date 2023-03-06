504f9b012d2c3c840cc5258ccb5f54a825ceb376
Toluwanimi Durosomo <toluwanimidurosomo30@gmail.com>
2023-03-06 17:54:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["a Student of UNILAG", "the PRO of EHRMSA", "a Worker in HICC"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
